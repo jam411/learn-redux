73b3a48170393251d3d57dccc213252e398f2c25
Ryoji Shimizu <ryoji_shimizu@earthbrain.com>
2023-12-28 08:58:24 +0000

    create top page

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="nav-center">
+        <h3>Redux Shoppping</h3>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
